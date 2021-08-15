cb33ddc7f50807ad00a1accd2d29d98a3d64b9d5
ZHtouchs <war3zhuhe@gmail.com>
2021-08-15 17:13:22 +0800

    20210815

---
+++ src/com/huawei/leetcode/editor/cn/ErChaShuDeZuiJinGongGongZuXianLcof.java
@@ -0,0 +1,89 @@
+package com.huawei.leetcode.editor.cn;
+
+//给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。 
+//
+// 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（
+//一个节点也可以是它自己的祖先）。” 
+//
+// 例如，给定如下二叉树: root = [3,5,1,6,2,0,8,null,null,7,4] 
+//
+// 
+//
+// 
+//
+// 示例 1: 
+//
+// 输入: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+//输出: 3
+//解释: 节点 5 和节点 1 的最近公共祖先是节点 3。
+// 
+//
+// 示例 2: 
+//
+// 输入: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+//输出: 5
+//解释: 节点 5 和节点 4 的最近公共祖先是节点 5。因为根据定义最近公共祖先节点可以为节点本身。
+// 
+//
+// 
+//
+// 说明: 
+//
+// 
+// 所有节点的值都是唯一的。 
+// p、q 为不同节点且均存在于给定的二叉树中。 
+// 
+//
+// 注意：本题与主站 236 题相同：https://leetcode-cn.com/problems/lowest-common-ancestor-of-a
+//-binary-tree/ 
+// Related Topics 树 深度优先搜索 二叉树 
+// 👍 297 👎 0
+
+//
+
+public class ErChaShuDeZuiJinGongGongZuXianLcof {
+    public static void main(String[] args) {
+        Solution solution = new ErChaShuDeZuiJinGongGongZuXianLcof().new Solution();
+
+    }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            if (root == null) return null; // 如果树为空，直接返回null
+            if (root == p || root == q) return root; // 如果 p和q中有等于 root的，那么它们的最近公共祖先即为root（一个节点也可以是它自己的祖先）
+            TreeNode left = lowestCommonAncestor(root.left, p, q); // 递归遍历左子树，只要在左子树中找到了p或q，则先找到谁就返回谁
+            TreeNode right = lowestCommonAncestor(root.right, p, q); // 递归遍历右子树，只要在右子树中找到了p或q，则先找到谁就返回谁
+            if (left == null) return right; // 如果在左子树中 p和 q都找不到，则 p和 q一定都在右子树中，右子树中先遍历到的那个就是最近公共祖先（一个节点也可以是它自己的祖先）
+            else if (right == null)
+                return left; // 否则，如果 left不为空，在左子树中有找到节点（p或q），这时候要再判断一下右子树中的情况，如果在右子树中，p和q都找不到，则 p和q一定都在左子树中，左子树中先遍历到的那个就是最近公共祖先（一个节点也可以是它自己的祖先）
+            else return root; //否则，当 left和 right均不为空时，说明 p、q节点分别在 root异侧, 最近公共祖先即为 root
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+//        （1） 如果当前结点 rootroot 等于 NULL，则直接返回 NULL
+//        （2） 如果 rootroot 等于 pp 或者 qq ，那这棵树一定返回 pp 或者 qq
+//        （3） 然后递归左右子树，因为是递归，使用函数后可认为左右子树已经算出结果，用 leftleft 和 rightright 表示
+//        （4） 此时若leftleft为空，那最终结果只要看 rightright；若 rightright 为空，那最终结果只要看 leftleft
+//        （5） 如果 leftleft 和 rightright 都非空，因为只给了 pp 和 qq 两个结点，都非空，说明一边一个，因此 rootroot 是他们的最近公共祖先
+//        （6） 如果 leftleft 和 rightright 都为空，则返回空（其实已经包含在前面的情况中了）
+//
+//        时间复杂度是 O(n)O(n)：每个结点最多遍历一次或用主定理，空间复杂度是 O(n)O(n)：需要系统栈空间
+//
+//        代码
+//
+//        作者：Wilson79
+//        链接：https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-tree/solution/c-jing-dian-di-gui-si-lu-fei-chang-hao-li-jie-shi-/
+//        来源：力扣（LeetCode）
+//        著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+}
--- src/com/huawei/leetcode/editor/cn/PingHengErChaShuLcof.java
+++ src/com/huawei/leetcode/editor/cn/PingHengErChaShuLcof.java
@@ -47,28 +47,63 @@ package com.huawei.leetcode.editor.cn;
 // Related Topics 树 深度优先搜索 二叉树 
 // 👍 180 👎 0
 
+import javax.swing.event.HyperlinkEvent;
+import java.util.HashMap;
 
-public class PingHengErChaShuLcof{
-	public static void main(String[] args) {
-		Solution solution = new PingHengErChaShuLcof().new Solution();
-		
-	}
-	
-//leetcode submit region begin(Prohibit modification and deletion)
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
-class Solution {
-    public boolean isBalanced(TreeNode root) {
-
+public class PingHengErChaShuLcof {
+    public static void main(String[] args) {
+        Solution solution = new PingHengErChaShuLcof().new Solution();
+        TreeNode treeNode = new TreeNode(1);
+        treeNode.left = new TreeNode(2);
+        treeNode.right = new TreeNode(3);
+        treeNode.right.left = new TreeNode(4);
+        treeNode.right.right = new TreeNode(5);
+        treeNode.right.right.left = new TreeNode(6);
+        treeNode.right.right.right = new TreeNode(7);
+        System.out.println(solution.isBalanced(treeNode));
     }
-}
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+        HashMap<TreeNode, Integer> integerHashMap = new HashMap<>();
+
+        public boolean isBalanced(TreeNode root) {
+            if (root == null) {
+                return true;
+            }
+            int left = getDepth(root.left);
+            int right = getDepth(root.right);
+
+            return Math.abs(right - left) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+        }
+
+        private int getDepth(TreeNode node) {
+            if (integerHashMap.containsKey(node)) {
+                return integerHashMap.get(node);
+            }
+            if (node == null) {
+                integerHashMap.put(null, 0);
+                return 0;
+            }
+            if (node.left == null && node.right == null) {
+                integerHashMap.put(node, 1);
+                return 1;
+            }
+            int depth = Math.max(getDepth(node.left), getDepth(node.right)) + 1;
+            integerHashMap.put(node, depth);
+            return depth;
+        }
+    }
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
