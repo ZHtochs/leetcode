0d68a51f8d07cb852501210311f97469c7ac0017
ZHtouchs <war3zhuhe@gmail.com>
2021-11-15 22:38:32 +0800

    20211115

---
+++ src/com/huawei/leetcode/editor/cn/P219ContainsDuplicateIi.java
@@ -0,0 +1,58 @@
+//给定一个整数数组和一个整数 k，判断数组中是否存在两个不同的索引 i 和 j，使得 nums [i] = nums [j]，并且 i 和 j 的差的 绝对值
+// 至多为 k。 
+//
+// 
+//
+// 示例 1: 
+//
+// 输入: nums = [1,2,3,1], k = 3
+//输出: true 
+//
+// 示例 2: 
+//
+// 输入: nums = [1,0,1,1], k = 1
+//输出: true 
+//
+// 示例 3: 
+//
+// 输入: nums = [1,2,3,1,2,3], k = 2
+//输出: false 
+// 👍 336 👎 0
+
+package com.huawei.leetcode.editor.cn;
+
+import java.util.HashMap;
+import java.util.function.BiFunction;
+
+//Java：存在重复元素 II
+public class P219ContainsDuplicateIi {
+    public static void main(String[] args) {
+        Solution solution = new P219ContainsDuplicateIi().new Solution();
+        System.out.println(solution.containsNearbyDuplicate(new int[]{1, 2, 3, 1}, 3));
+        System.out.println(solution.containsNearbyDuplicate(new int[]{1, 0, 1, 1}, 1));
+        System.out.println(solution.containsNearbyDuplicate(new int[]{1, 2, 3, 1, 2, 3}, 2));
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public boolean containsNearbyDuplicate(int[] nums, int k) {
+            HashMap<Integer, Integer> hashMap = new HashMap<>();
+            for (int i = 0; i < nums.length; i++) {
+                int current = nums[i];
+                if (hashMap.containsKey(current)) {
+                    int lastPosition = hashMap.get(current);
+                    if (i - lastPosition <= k) {
+                        return true;
+                    } else {
+                        hashMap.put(current, i);
+                    }
+                } else {
+                    hashMap.put(current, i);
+                }
+            }
+            return false;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
